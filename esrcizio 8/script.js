b78cf78fbb42809546a589784e6e441e0b74f690
mirko prisciano <mirkoprisciano@gmail.com>
2025-04-11 23:02:58 +0200

    sfondo e bordo

---
+++ esrcizio 8/script.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#1E40AF",
+          secondary: "#FACC15",
+          accent: "#F43F5E",
+          background: "#E5E7EB",
+          borderColor: "#9333EA",
+        },
+      },
+    },
+    plugins: [],
+  };
+  
