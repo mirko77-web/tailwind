646a40992d55d51687016a780a1bd6175201dcba
mirko prisciano <mirkoprisciano@gmail.com>
2025-04-11 22:42:39 +0200

    palette custom

---
+++ esercizio 5/script.js
@@ -0,0 +1,15 @@
+@type {import('tailwindcss').Config}
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                customRed: "#E63946",
+                customAmber: "#FFA62B",
+                customBlue: "#457B9D",
+                customGreen: "#2A9D8F",
+                customPurple: "#6A0572"
+            }
+        }
+    },
+    plugins: [],
+};
